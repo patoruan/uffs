47797442c88da1526e3fbecb3b92e02ecc56774e
ruanpato <patoruanrecup@gmail.com>
2019-03-12 23:32:34 -0300

    nothing yet

--- TAP/2Lista/2018uri.cpp
+++ TAP/2Lista/2018uri.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <map>
+#include <iterator>
 
 using namespace std;
 
 int main(){
+	map<std::string, pair<int, pair<int, int> > > countryMedals;
+	char countryName[11234];
+	while(scanf("%s", countryName) != EOF){
+		//Gold medal
+		scanf("%s", countryName);
+		
+	}
 
 	return 0;
 }

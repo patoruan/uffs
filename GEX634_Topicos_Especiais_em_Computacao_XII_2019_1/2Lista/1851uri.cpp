0377c4547a8c5b9d288761ffeda2281942fb70ed
ruanpato <patoruanrecup@gmail.com>
2019-05-05 18:05:57 -0300

    1851

--- GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/1851uri.cpp
+++ GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/1851uri.cpp
@@ -1,12 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <queue>
-//Priority Queue
-//Daenerys can't stay in any moment without training dragons
+#include <tuple>
+#include <vector>
+
 using namespace std;
 
+typedef long long int lli;
+typedef tuple<double,lli,lli,lli> tllll;
+priority_queue<tllll, vector<tllll>, greater<tllll>> pq, auxPq;// int dia*multa para a priority, int Dia chegada, Dias para treino, multa por dias não treinado em espera
+
 int main(){
-	unsigned long long int multa;
+	tllll aux;
+	lli t, f, tot = 0, totDias=1, dEmtreino=0;
+
+	while(cin >> t >> f){
+		pq.push(make_tuple(((double)t/(double)f), totDias, t, f));
+		if(dEmtreino==0){
+			aux=pq.top();pq.pop();// Coloca o valor da fila de prioridade em uma tupla auxiliar e remove da lista de prioridade
+			tot += ( (totDias-get<1>(aux))*get<3>(aux) );// Aumenta o total da multa
+
+			dEmtreino=get<2>(aux);//Atualiza os dias de espera
+		}totDias++;dEmtreino--;
+	}
 	
+	while(!pq.empty()){
+		if(dEmtreino==0){
+			aux=pq.top();pq.pop();// Coloca o valor da fila de prioridade em uma tupla auxiliar e remove da lista de prioridade
+			tot += ( (totDias-get<1>(aux))*get<3>(aux) );// Aumenta o total da multa
+
+			dEmtreino=get<2>(aux);//Atualiza os dias de espera
+		}totDias++;dEmtreino--;
+	}
+
+	cout << tot << endl;
 	return 0;
 }
--- GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/README.md
+++ GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/README.md
@@ -4,7 +4,7 @@
 * [URI Online Judge](https://www.urionlinejudge.com.br)
   * **Problems:**
     * [1215](https://www.urionlinejudge.com.br/judge/pt/problems/view/1215) - Primeiro Dicionário de Andy - [[Solução]](https://github.com/ruanpato/uffs/blob/master/GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/1215uri.cpp)
-    * [1851](https://www.urionlinejudge.com.br/judge/pt/problems/view/1851) - Como Treinar Seu Dragão
+    * [1851](https://www.urionlinejudge.com.br/judge/pt/problems/view/1851) - Como Treinar Seu Dragão - [[Solução]](https://github.com/ruanpato/uffs/blob/master/GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/1851uri.cpp)
     * [1861](https://www.urionlinejudge.com.br/judge/pt/problems/view/1861) - O Hall dos Assassinos - [[Solução 0]](https://github.com/ruanpato/uffs/blob/master/GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/1861uri.cpp) - [[Solução 1]](https://github.com/ruanpato/uffs/blob/master/GEX634_Topicos_Especiais_em_Computacao_XII_2019_1/2Lista/1861.cpp)
     * [2018](https://www.urionlinejudge.com.br/judge/pt/problems/view/2018) - Olimpíadas de Natal
     
